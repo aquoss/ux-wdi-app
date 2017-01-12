1035fedacec18d096175157dc0ccf35cbf324391
aquoss <amberquoss@gmail.com>
2017-01-12 14:38:29 -0800

    initializes homepage controller

---
+++ public/scripts/controllers/HomepageController.js
@@ -0,0 +1,8 @@
+angular.module('feedable')
+       .controller('HomepageController', HomepageController);
+
+HomepageController.$inject = [];
+function HomepageController(){
+  var vm = this;
+  
+}
