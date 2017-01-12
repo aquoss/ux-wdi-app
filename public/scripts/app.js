4ae97a5d78bea9eae9b391473b5120bb2c5d1281
aquoss <amberquoss@gmail.com>
2017-01-12 15:49:27 -0800

    creates backend and attatches templates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,28 +5,28 @@ config.$inject = ['$routeProvider', '$locationProvider'];
 function config($routeProvider, $locationProvider){
   $routeProvider
     .when('/', {
-      templateUrl: 'templates/homepage.html',
+      templateUrl: '/templates/homepage',
       controller: 'HomepageController',
       controllerAs: 'homepageCtrl'
     })
-    .when('/restaurant', {
-      templateUrl: 'templates/restaurant-info.html',
-      controller: 'RestaurantInfoController',
-      controllerAs: 'restaurantInfoCtrl'
-    })
-    // .when('/restaurant/pickup', {
-    //
+    // .when('/restaurant', {
+    //   templateUrl: '/templates/restaurant-info',
+    //   controller: 'RestaurantInfoController',
+    //   controllerAs: 'restaurantInfoCtrl'
     // })
-    // .when('/restaurant/drop-off', {
-    //
-    // })
-    .otherwise({
-      redirectTo: '/'
-    });
+    // // .when('/restaurant/pickup', {
+    // //
+    // // })
+    // // .when('/restaurant/drop-off', {
+    // //
+    // // })
+    // .otherwise({
+    //   redirectTo: '/'
+    // });
 
     $locationProvider.html5Mode({
       enabled: true,
-      requiredBase: false
+      requireBase: false
     });
 
 }
---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+    bodyParser = require('body-parser');
+
+// var db = require('./models');
+
+var app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function (req,res){
+  res.sendFile('views/index.html', {root:__dirname});
+})
+
+app.get('/templates/:name', function templates(req, res) {
+  var name = req.params.name;
+  res.sendFile(__dirname + '/views/templates/' + name + '.html');
+});
+
+// CREATE ANY NECESSARY ROUTES
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log('Example app listening at http://localhost:3000/');
+})
