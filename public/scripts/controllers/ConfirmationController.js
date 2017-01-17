5bf4ae8035321ebb0a486cd7a45bc184d40a171e
aquoss <amberquoss@gmail.com>
2017-01-16 23:51:34 -0800

    deletes unecessary controller and html

--- public/scripts/controllers/ConfirmationController.js
+++
@@ -1,7 +0,0 @@
-angular.module('feedable')
-       .controller('ConfirmationController', ConfirmationController);
-
-ConfirmationController.$inject = [];
-function ConfirmationController(){
-  var vm=this;
-}
