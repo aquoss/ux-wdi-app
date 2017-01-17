a4f944540b0b26625e92b47188bede2dbbbdf439
aquoss <amberquoss@gmail.com>
2017-01-16 20:22:10 -0800

    creates list of restaurants with search bar

--- public/scripts/controllers/RestaurantListController.js
+++ public/scripts/controllers/RestaurantListController.js
@@ -1,10 +1,10 @@
 angular.module('feedable')
        .controller('RestaurantListController', RestaurantListController);
 
-RestaurantListController.$inject=['$http'];
-function RestaurantListController($http){
-  var vm=this;
-
+RestaurantListController.$inject=['$http','$location'];
+function RestaurantListController($http, $location){
+  var vm = this;
+  vm.restaurants = {};
   $http({
     method: 'GET',
     url: '/restaurants'
@@ -14,5 +14,9 @@ function RestaurantListController($http){
   }, function errorCallback(response) {
     console.log('There was an error getting the data', response);
   });
+
+  vm.goBack = function(){
+    $location.path('/');
+  }
 
 }
