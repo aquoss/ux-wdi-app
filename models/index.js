b7fc38c731da0a90ab5f32c7e11d6f68a1717600
aquoss <amberquoss@gmail.com>
2017-01-17 09:49:17 -0800

    adds heroku connection

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/feedable');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/feedable');
 
 module.exports.Restaurant = require('./restaurants.js');
+// module.exports.Request
