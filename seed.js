bd82e2b9da36c1dac461eff8e252a6e7b855a75d
aquoss <amberquoss@gmail.com>
2017-01-22 17:26:03 -0800

    creates seed data for soup kitchens

--- seed.js
+++ seed.js
@@ -48,3 +48,57 @@ db.Restaurant.remove({}, function(err, restaurants){
   });
 
 });
+
+var kitchenList = [];
+kitchenList.push({
+              name: 'San Francisco Food Bank',
+              address: '900 Pennsylvania Ave, San Francisco, CA 94107',
+              phone: '(415) 282-1900',
+              hours: 'M-F: 8am-5pm, Sat/Sun: 9am-3:30pm',
+              location: {lat: 37.7544354, lng:-122.3935173}
+            });
+kitchenList.push({
+              name: 'Project Open Hand',
+              address: '730 Polk St, San Francisco, CA 94109',
+              phone: '(415) 447-2300',
+              hours: 'Tues: 9am-4pm, Wed-Sat: 11am-4pm',
+              location: {lat: 37.7837976, lng:-122.4280263}
+            });
+kitchenList.push({
+              name: 'North Peninsula Food Pantry',
+              address: '31 Bepler St, Daly City, CA 94014',
+              phone: '(650) 994-5150',
+              hours: 'Mon, Tues, Thurs: 3-5:30pm',
+              location: {lat: 37.7065506, lng:-122.47076}
+            });
+kitchenList.push({
+              name: 'St. Anthony Foundation',
+              address: '150 Golden Gate Ave, San Francisco, CA 94102',
+              phone: '(415) 241-2600',
+              hours: 'call to inquire',
+              location: {lat: 37.7866977, lng:-122.4184426}
+            });
+kitchenList.push({
+              name: 'North Beach Citizens',
+              address: '1034 Kearny St, San Francisco, CA 94133',
+              phone: '(415) 772-0918',
+              hours: 'M-Sat: 9:30am-12pm',
+              location: {lat: 37.7974611, lng:-122.4054017}
+            });
+kitchenList.push({
+              name: 'California Association of Food Banks',
+              address: '1624 Franklin St #722, Oakland, CA 94612',
+              phone: '(510) 272-4435',
+              hours: 'Tues-Sat: 10am-5pm',
+              location: {lat: 37.8058038, lng:-122.2684058}
+            });
+
+db.SoupKitchen.remove({}, function(err, kitchens){
+
+  db.SoupKitchen.create(kitchenList, function(err, kitchens){
+    if (err) { return console.log('ERROR', err); }
+    console.log("all soup-kitchens:", kitchens);
+    process.exit();
+  });
+
+});
